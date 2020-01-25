fac2b534f1a920b1bb5b87bf6c7fa894da29ad33
Olga <baushonok@gmail.com>
2020-01-25 16:50:44 +0300

    Add module.exports

--- check-errors.js
+++ check-errors.js
@@ -1,4 +1,4 @@
-export function isRegExp(value, valueName) {
+function isRegExp(value, valueName) {
   if (!(value instanceof RegExp)) {
     throw new Error(
       `Expected ${valueName} to be regular expression but got ${value}`
@@ -6,13 +6,13 @@ export function isRegExp(value, valueName) {
   }
 }
 
-export function isString(value, valueName) {
+function isString(value, valueName) {
   if (typeof value !== "string") {
     throw new Error(`Expected ${valueName} to be string but got ${value}`);
   }
 }
 
-export function isArray(value, valueName) {
+function isArray(value, valueName) {
   if (!Array.isArray(value)) {
     throw new Error(
       `Expected ${valueName} to be an array of string but got ${value}`
@@ -20,8 +20,15 @@ export function isArray(value, valueName) {
   }
 }
 
-export function isFunction(value, valueName) {
+function isFunction(value, valueName) {
   if (typeof value !== "function") {
     throw new Error(`Expected ${valueName} to be a function but got ${value}`);
   }
 }
+
+module.exports = {
+  isArray,
+  isFunction,
+  isRegExp,
+  isString
+};
